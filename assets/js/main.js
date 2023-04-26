e7ca359ed44bd9a63d7b9bb1e7c168059a30cfb7
LaloAlex3 <laloalex3@gmail.com>
2023-04-25 23:43:58 -0600

    Save changes 25-04-2023 23:43hrs

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+gsap.registerPlugin(ScrollTrigger);
+
+// const images = gsap.utils.toArray("img");
+
+gsap.utils.toArray("section").forEach((section, index) => {
+  const w = section.querySelector(".images");
+  const [x, xEnd] = [w.scrollWidth * -1, 0];
+  gsap.fromTo(
+    w,
+    { x },
+    {
+      x: xEnd,
+      scrollTrigger: {
+        trigger: section,
+        scrub: 1,
+      },
+    }
+  );
+});
